83143d663ee89260d397d41090c435a608a9502b
Harprittt <ebsharpreetkaur@gmail.com>
2023-06-02 09:40:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import '../src/';
+import Sitelayout from './components/Sitelayout';
+
+
+function App() {
+  return (
+    // <Login />
+    <Sitelayout />
+  );
+}
+
+export default App;
---
+++ src/components/Sitelayout.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+const { Header, Sider, Content } = Layout;
+
+
+export default function Sitelayout() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed} className='siderstyle'>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Dashboard',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'Leave',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'Books',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  )
+}
